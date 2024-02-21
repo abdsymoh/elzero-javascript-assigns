57e351e0a0d406baeb9dcb7ec02209c653ddf883
abdsymoh <abdsydev@gmail.com>
2024-02-21 09:33:49 +0200

    solve assignments for lessons 10 - 17

---
+++ 10-17/assignments.js
@@ -0,0 +1,30 @@
+// assign 1
+console.log("==== assign 1 ====");
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log(numberOne + "" + numberTwo);
+console.log(typeof (numberOne + "" + numberTwo));
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof `${numberOne}${numberTwo}`);
+console.log(numberTwo + "\n" + numberOne);
+console.log(`${numberTwo}
+${numberOne}`);
+console.log("==================");
+
+// assign 2
+console.log("==== assign 2 ====");
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+console.log("==================");
+
+// assign 3
+console.log("==== assign 3 ====");
+console.log('`I\'m In\n\\\\\nLove \\""" \'\'\'\n++ with ++\n\\ """\\ """\n""JavaScript""``');
+console.log("==================");
+
+// assign 4
+console.log("==== assign 4 ====");
+let a = 21;
+let b = 20;
+console.log("_" + a + `_${b}${a}`.repeat(3) + "_" + b + "_");
