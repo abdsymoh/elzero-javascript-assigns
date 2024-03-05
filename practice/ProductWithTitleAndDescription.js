6f4bb7151f21c4a6e224fb004b035e15a4515ed7
abdsymoh <abdsydev@gmail.com>
2024-03-05 10:43:52 +0200

    make it 100 products

--- practice/ProductWithTitleAndDescription.js
+++ practice/ProductWithTitleAndDescription.js
@@ -3,30 +3,36 @@
   - Practice Product With Heading And Paragraph
 */
 
-// Create Elements
-let productCard = document.createElement("div");
-let productTitle = document.createElement("h2");
-let productDescription = document.createElement("p");
+for (i = 0; i < 100; i++) {
+  // Create Elements
+  let productCard = document.createElement("div");
+  let productTitle = document.createElement("h2");
+  let productDescription = document.createElement("p");
+  let hr = document.createElement("hr")
 
-// Create text for the title and description
-let productTitleText = document.createTextNode("Product Title");
-let productDescriptionText = document.createTextNode("Description");
+  // Create text for the title and description
+  let productTitleText = document.createTextNode(`Product Title ${i + 1}`);
+  let productDescriptionText = document.createTextNode("Description");
 
-// add the text to the title and description
-productTitle.appendChild(productTitleText);
-productDescription.appendChild(productDescriptionText);
+  // add the text to the title and description
+  productTitle.appendChild(productTitleText);
+  productDescription.appendChild(productDescriptionText);
 
-// add the title to the card
-productCard.appendChild(productTitle);
+  // add the title to the card
+  productCard.appendChild(productTitle);
 
-// add the description to the card
-productCard.appendChild(productDescription);
+  // add the description to the card
+  productCard.appendChild(productDescription);
 
-// add Classes to the Elements
-productCard.className = "product-card"
-productTitle.className = "product-title"
-productDescription.className = "product-desc"
+  // add the hr to the card
+  productCard.appendChild(hr)
 
-// add the productCard to the body
+  // add Classes to the Elements
+  productCard.className = "product-card";
+  productTitle.className = "product-title";
+  productDescription.className = "product-desc";
 
-document.body.appendChild(productCard)
+  // add the productCard to the body
+
+  document.body.appendChild(productCard);
+}
