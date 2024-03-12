f9c5b9a6a09cf42cc267980880c7a0700a38397f
abdsymoh <abdsydev@gmail.com>
2024-03-12 09:32:54 +0200

    scroll to top practice

---
+++ practice/scrolltotop.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("button");
+window.onscroll = function () {
+  if (window.scrollY >= "600") {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
