22c3b841490c0c29f276302b2575c39e98ba6639
abdsymoh <abdsydev@gmail.com>
2024-03-30 14:05:20 +0200

    practice on Array.every()

--- practice/ArrayDot.js
+++ practice/ArrayDot.js
@@ -7,22 +7,31 @@ console.log(myArray.copyWithin(1, -2, -1)); // [10, "A", 30, 40, 50, "A", "B"]
 
 // Array.some()
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let check = nums.some(function (e) {
-  console.log("بسم الله الرحمن الرحيم");
-  return e > 5;
-});
-// let check = nums.some((e) => e < 5);
-console.log(check);
-
 console.log("#".repeat(20));
-
 let range = {
   min: 10,
   max: 20,
 };
-
 let checkNumberInRange = nums.some(function (e) {
   return e >= this.min && e <= this.max;
 }, range);
-
 console.log(checkNumberInRange);
+console.log("#".repeat(20));
+
+// Array.every()
+
+const locations = {
+  20: "Place 1",
+  30: "Place 2",
+  10: "Place 3",
+  40: "Place 4",
+};
+let mainLocation = 15;
+let locationsArray = Object.keys(locations).map((n) => +n);
+
+let check = locationsArray.every(function (n) {
+  return n > this;
+}, mainLocation);
+
+console.log(locationsArray);
+console.log(check);
