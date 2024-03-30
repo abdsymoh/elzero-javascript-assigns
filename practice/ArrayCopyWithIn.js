9b9c5a6d513830be1ade3629448901c682672514
abdsymoh <abdsydev@gmail.com>
2024-03-30 13:49:19 +0200

    practice on Array.Some()

--- practice/ArrayCopyWithIn.js
+++
@@ -1,3 +0,0 @@
-let myArray = [10, 20, 30, 40, 50, "A", "B"];
-// console.log(myArray.copyWithin(1, -2)); // [10, "A", "B", 40, 50, "A", "B"]
-console.log(myArray.copyWithin(1, -2, -1)); // [10, "A", 30, 40, 50, "A", "B"]
